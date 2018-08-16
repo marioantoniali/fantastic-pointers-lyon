abd10aa3f735bf0b2ca0b9df3aecdf09cbf2206a
Pauline <mya@mya-ow.net>
2018-08-16 16:34:34 +0200

    fleche

--- app/assets/javascripts/bouton.js
+++ app/assets/javascripts/bouton.js
@@ -1,5 +1,5 @@
-$(function(){
-  $("#monBouton").click(function(){
-      $("html, body").animate({scrollTop: 0},"slow");
-  });
-});
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
